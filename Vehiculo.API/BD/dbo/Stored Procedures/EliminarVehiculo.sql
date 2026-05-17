e7b69ea1d374b6f5e21cf0ab06b8cbee4195a383
gabot <gabot@HENRYLAPTOP>
2026-05-17 10:32:51 -0600

    Semana 2 video 1

---
+++ Vehiculo.API/BD/dbo/Stored Procedures/EliminarVehiculo.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE EliminarVehiculo 
+	@Id uniqueidentifier
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DELETE
+	FROM   Vehiculo 
+	WHERE (Id = @Id)
+END
